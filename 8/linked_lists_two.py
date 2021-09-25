6d7a1904c6f1947015ab17e6f03bc4247e42b7c3
Michael Mutuku <themikelyway.gmail.com>
2021-09-25 12:25:08 +0300

    linked list creation and insertion

---
+++ 8/linked_lists_two.py
@@ -0,0 +1,50 @@
+# Insertion into a singly linked list
+
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+
+    def insertSSL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                nextNode.next = nextNode
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSSL(1,1)
+singlyLinkedList.insertSSL(2,1)
+singlyLinkedList.insertSSL(3,1)
+singlyLinkedList.insertSSL(4,1)
+
+singlyLinkedList.insertSSL(0,0)
+singlyLinkedList.insertSSL(0,3)
+print([node.value for node in singlyLinkedList])
