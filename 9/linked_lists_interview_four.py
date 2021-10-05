8b9f6b18e1c439db5972571b24f8e519ac85e8b0
Michael Mutuku <themikelyway.gmail.com>
2021-10-05 13:28:35 +0300

    linked list interview

---
+++ 9/linked_lists_interview_four.py
@@ -0,0 +1,29 @@
+# Partition
+
+from linked_lists_interview_one import LinkedList
+
+def partition(ll, x):
+    curNode = ll.head
+    ll.tail = ll.head
+
+    while curNode:
+        nextNode = curNode.next
+        curNode.next = None
+        if curNode.value < x:
+            curNode.next = ll.head
+            ll.head = curNode
+        else:
+            ll.tail.next = curNode
+            ll.tail = curNode
+        curNode = nextNode
+
+    if ll.tail.next is not None:
+        ll.tail.next = None
+
+customLL = LinkedList()
+customLL.generate(10, 0, 99)
+print(customLL)
+partition(customLL, 30)
+print(customLL)
+
+# code worked as expected
