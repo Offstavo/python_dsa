cf6248d02be5f1ebe04ae139e2f6de8fb2ea3b9b
Michael Mutuku <themikelyway.gmail.com>
2021-10-07 18:46:45 +0300

    stacks

---
+++ 10/stacks_one.py
@@ -0,0 +1,12 @@
+# Stack creation using lists without size limit
+
+class Stack:
+    def __init__(self):
+        self.list = []
+
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
+
+# skeleton code no valid output
