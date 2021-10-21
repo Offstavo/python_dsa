994e681ec108c15ce4c16c134aa7ceb47770bda9
Michael Mutuku <themikelyway.gmail.com>
2021-10-21 17:19:24 +0300

    graphs intro

---
+++ 20/graph_one.py
@@ -0,0 +1,25 @@
+# Create a graph
+
+class Graph:
+    def __init__(self, gdict=None):
+        if gdict is None:
+            gdict = {}
+        self.gdict = gdict
+
+    def addEdge(self, vertex, edge):
+        self.gdict[vertex].append(edge)
+
+customDict = { "a" : ["b","c"],
+               "b" : ["a","d","e"],
+               "c" : ["a","e"],
+               "d" : ["b","e","f"],
+               "e" : ["d","f"],
+               "f" : ["d","e"]
+                }
+
+graph = Graph(customDict)
+graph.addEdge("e", "c")
+print(graph.gdict)
+print(graph.gdict["e"])
+
+# code worked as expected
